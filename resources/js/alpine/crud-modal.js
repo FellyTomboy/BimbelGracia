5fbe481786f7a5f6bbfc6324a05c804b8761a517
Felicita Arcadia Agis Rynawan <flc982006@gmail.com>
2026-09-15 19:22:06 +0700

    2026-09-15 19:22 - updated via Claude

--- resources/js/alpine/crud-modal.js
+++ resources/js/alpine/crud-modal.js
@@ -221,7 +221,18 @@ export function crudModal(config) {
                 el.classList.add('border-gray-300');
             });
 
-            const params = new URLSearchParams(new FormData(form));
+            const params = new URLSearchParams();
+            for (const el of form.querySelectorAll('input[name], select[name], textarea[name]')) {
+                if (el.type === 'checkbox') {
+                    if (el.checked) params.append(el.name, el.value || 'on');
+                } else if (el.type === 'radio') {
+                    if (el.checked) params.append(el.name, el.value);
+                } else if (el.tagName === 'SELECT' && el.multiple) {
+                    for (const opt of el.selectedOptions) params.append(el.name, opt.value);
+                } else if (el.type === 'hidden' || el.value) {
+                    params.append(el.name, el.value);
+                }
+            }
 
             const url = this.isEdit
                 ? (typeof this.updateUrl === 'function' ? this.updateUrl(this.currentId) : `${this.updateUrl}/${this.currentId}`)
